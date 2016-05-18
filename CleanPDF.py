a66ae6818d54a4c475f01ebba48e3a400fc43fb0
marcelocavieresd <marcelocavieresd@gmail.com>
2016-05-18 20:14:02 -0300

    Update CleanPDF.py

--- CleanPDF.py
+++ CleanPDF.py
@@ -1,15 +1,14 @@
-"""Script developed by Marcelo Cavieres"""
+"""Script developed by Marcelo Cavieres, Unab student, Santiago-Chile"""
 
 print "Note: This script take as root folder in the running"
 ruta = raw_input("From: ")
 rutadestino = raw_input("To: ")
-try:
-    f = open(ruta, "r")
-    d = open(rutadestino, "w")
-    for x in f:
-        if "/Producer" not in x:
-            d.write(x)
-    d.close()
-    f.close()
-    print "*** Metadata were deleted ***"
-    print "Clean file was saved in: ",rutadestino
+f = open(ruta, "r")
+d = open(rutadestino, "w")
+for x in f:
+    if "/Producer" not in x:
+        d.write(x)
+d.close()
+f.close()
+print "*** Metadata were deleted ***"
+print "Clean file was saved in: ",rutadestino
